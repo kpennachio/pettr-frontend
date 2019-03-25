1d294d3746d84ba157afd187fa96546277a93eee
cvolpe12 <christopher.volpe12@gmail.com>
2019-03-25 17:53:58 -0400

    pictures upload

--- src/PetProfile.js
+++ src/PetProfile.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import Athena from './Pet_Pictures/Athena.JPG'
 
 export default class PetProfile extends Component {
 
   render() {
     console.log(this.props.pet.picture)
-
+    // debugger
     return(
       <div>
-        <img src={this.props.pet.picture}></img>
+
       </div>
     )
   }
 }
+
+// render for pet imgs
+// <img src={require(`${this.props.pet.picture}`)}/>
