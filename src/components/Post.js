fb1fa284d696c25fa98561a8c2aaa794e111222f
cannoli9917 <katie.pennachio@gmail.com>
2019-03-26 15:36:43 -0400

    created post form and make new post with string content

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,8 +4,8 @@ class Post extends React.Component {
 
   render() {
     return (
-      <div>
-        <h4>Show Posts</h4>
+      <div className="post">
+        <p>{this.props.post.content}</p>
       </div>
     )
   }
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -2,10 +2,25 @@ import React from "react";
 
 class PostForm extends React.Component {
 
+  state = {
+    value: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+
   render() {
     return (
-      <div>
-        <input type="text" name="form" placeholder="new post"/>
+      <div className="form-container">
+        <h2>Create a New Post</h2>
+        <form onSubmit={(e) => this.props.createNewPost(e, this.state.value)}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
