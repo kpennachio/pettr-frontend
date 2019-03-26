fb895482545bcd7d1690aa898ad821eef1b89580
cannoli9917 <katie.pennachio@gmail.com>
2019-03-26 18:17:59 -0400

    start of cloudinary

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,7 +3,8 @@ import React from "react";
 class PostForm extends React.Component {
 
   state = {
-    value: ""
+    value: "",
+    image: ""
   }
 
   handleChange = (e) => {
@@ -12,13 +13,26 @@ class PostForm extends React.Component {
     })
   }
 
+  seeResult = (result) => {
+    if (result.event === "success") {
+      this.setState({image: result.info.secure_url})
+    }
+  }
+
+  openWidget = () => {
+    let widget = window.cloudinary.createUploadWidget({
+      cloudName: process.env.REACT_APP_CLOUD_NAME, uploadPreset: process.env.REACT_APP_UPLOAD_PRESET }, (error, result) => {this.seeResult(result) });
+    widget.open();
+
+  }
 
   render() {
     return (
       <div className="form-container">
         <h2>Create a New Post</h2>
         <form onSubmit={(e) => this.props.createNewPost(e, this.state.value)}>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          <button onClick={this.openWidget}>Select Image</button>
           <input type="submit" value="Submit" />
         </form>
       </div>
